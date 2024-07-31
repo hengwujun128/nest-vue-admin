f3b7f7f1c5e173b550816fd2da24438796fb6beb
martin <hengwujun128@gmail.com>
2024-07-31 17:04:18 +0800

    feat: add ROUTES_MAP for routes

--- src/router/routes/route.map.ts
+++ src/router/routes/route.map.ts
@@ -1,5 +1,5 @@
 /**
- * 路由映射
+ * 路由映射: name => component
  */
 
 import { asyncRoutes } from '@/router/routes/index'
@@ -16,5 +16,5 @@ export const generateRouteMap = (routes) => {
 
 // 生成路由映射
 generateRouteMap(asyncRoutes)
-
+console.log(newRoutes)
 export const ROUTES_MAP = newRoutes
