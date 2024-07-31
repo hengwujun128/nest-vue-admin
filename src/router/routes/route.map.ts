41673fe9e66632c07c814dc252bc0269fbc4666f
martin <hengwujun128@gmail.com>
2024-07-31 17:02:57 +0800

    feat: add ROUTES_MAP for routes

---
+++ src/router/routes/route.map.ts
@@ -0,0 +1,20 @@
+/**
+ * 路由映射
+ */
+
+import { asyncRoutes } from '@/router/routes/index'
+
+const newRoutes = {}
+export const generateRouteMap = (routes) => {
+  return routes.map((route) => {
+    if (route.children && route.children.length) {
+      generateRouteMap(route.children)
+    }
+    newRoutes[route.name] = route.component
+  })
+}
+
+// 生成路由映射
+generateRouteMap(asyncRoutes)
+
+export const ROUTES_MAP = newRoutes
